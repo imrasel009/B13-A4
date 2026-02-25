546f01bda62c0a67b111ace41d36738a9191e7b1
imrasel009 <imrsl009@gmail.com>
2026-02-25 16:03:34 +0600

    moving card to different sections

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let currentTab = 'all';
 const tabActive = ["bg-[#3B82F6]", "text-white"];
 const tabInactive = ["bg-white", "text-[#64748B]]"];
 
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById("interview-container");
+const rejectedContainer = document.getElementById("rejected-container");
+
+
+
+
 function switchTab(tab){
     console.log(tab);
     const tabs = ["all", "interview", "rejected"];
@@ -18,5 +25,54 @@ function switchTab(tab){
         }
 
     }
+
+    const pages = [allContainer, interviewContainer, rejectedContainer];
+
+    for(const section of pages){
+        section.classList.add("hidden");
+    }
     
+    if (tab==="all") {
+        allContainer.classList.remove("hidden");
+    }
+    else if(tab=== "interview"){
+        interviewContainer.classList.remove("hidden");
+    }
+    else{
+        rejectedContainer.classList.remove("hidden");
+    }
+        
 }
+
+// stat update
+const totalStat = document.getElementById("stat-total");
+const interviewStat =document.getElementById("stat-interview");
+const rejectedStat =document.getElementById("stat-rejected");
+
+totalStat.innerText = allContainer.children.length;
+
+
+switchTab(currentTab);
+
+document.getElementById("jobs-container").addEventListener("click",function(event){
+    const clickedElement = event.target;
+    const card = clickedElement.closest(".card");
+    const status = card.querySelector(".status");
+    
+
+    if (clickedElement.classList.contains("interview")) {
+        status.innerText = "Interviewed";
+        interviewContainer.appendChild(card);
+        
+    }
+    if (clickedElement.classList.contains("rejected")) {
+        status.innerText = "Rejected";
+        rejectedContainer.appendChild(card);
+        
+    }
+    if (clickedElement.closest(".delete")) {
+        // console.log("delete clicked");
+        
+    }
+    
+})
